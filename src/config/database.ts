656c321bedc38f0e0f2a4aa53b13df9b49dae21f
qq1103409364 <1103409364@qq.com>
2022-04-10 13:55:50 +0800

    更新 typeorm、ts，替换 mysql 驱动
    Signed-off-by: qq1103409364 <1103409364@qq.com>

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,8 +5,8 @@ export default {
   socketPath: '/tmp/mysql.sock',
   port: 3306,
   username: 'root',
-  password: 'xiaoerxiaoer',
-  database: 'nest',
+  password: 'admin123',
+  database: 'nest_demo',
   entities: [join(__dirname, '../', '**/**.entity{.ts,.js}')],
   synchronize: true,
 };
